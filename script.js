f0fa1c271f41eb3c9819f0ba13ff1d58b17d3b7d
Natalya <nne30@yandex.ru>
2024-06-26 12:29:48 +0300

    added changes

---
+++ script.js
@@ -0,0 +1,88 @@
+new Vue({
+  el: '#app',
+  data: {
+    articles: [
+      {
+        id: 1,
+        image: 'img/articles-photo1.png',
+        alt:'interior',
+        category: 'Дизайн кухни',
+        title: 'Создадим лучший макет перепланировки',
+        date: '26 December, 2022',
+        link: '#'
+      },
+      { id: 2, 
+        image: 'img/articles-photo2.png', 
+        alt: 'interior', category: 'Дизайн для жизни', 
+        title: 'Лучшие интерьерные идеи по низкой цене', 
+        date: '22 December, 2022', 
+        link: '#' },
+      { id: 3, 
+        image: 'img/articles-photo3.png', 
+        alt: 'interior', 
+        category: 'Дизайн Интерьера', 
+        title: 'Лучшие интерьерные решения для офисов', 
+        date: '25 December, 2022', 
+        link: '#' },
+    ]
+  }
+});
+
+new Vue({
+  el: '#app-blog',
+  data: {
+    articles: [
+      { 
+        id: 1,
+        image: 'img/articles-photo1.png',
+        alt:'interior',
+        category: 'Дизайн кухни',
+        title: 'Создадим лучший макет перепланировки',
+        date: '26 December, 2022',
+        link: '#'
+      },
+      { 
+        id: 2, 
+        image: 'img/articles-photo2.png', 
+        alt: 'interior', category: 'Дизайн для жизни', 
+        title: 'Лучшие интерьерные идеи по низкой цене', 
+        date: '22 December, 2022', 
+        link: '#' 
+      },
+      { 
+        id: 3, 
+        image: 'img/articles-photo3.png', 
+        alt: 'interior', 
+        category: 'Дизайн Интерьера', 
+        title: 'Лучшие интерьерные решения для офисов', 
+        date: '25 December, 2022', 
+        link: '#' 
+      },
+      { 
+        id: 4,
+        image: 'img/articles-photo4.png',
+        alt:'interior',
+        category: 'Дизайн кухни',
+        title: 'Создадим лучший макет перепланировки',
+        date: '26 December, 2022',
+        link: '#'
+      },
+      { 
+        id: 5, 
+        image: 'img/articles-photo5.png', 
+        alt: 'interior', category: 'Дизайн для жизни', 
+        title: 'Лучшие интерьерные идеи по низкой цене', 
+        date: '22 December, 2022', 
+        link: '#' 
+      },
+      { 
+        id: 6, 
+        image: 'img/articles-photo6.png', 
+        alt: 'interior', 
+        category: 'Дизайн Интерьера', 
+        title: 'Лучшие интерьерные решения для офисов', 
+        date: '25 December, 2022', 
+        link: '#' },
+    ]
+  }
+})
